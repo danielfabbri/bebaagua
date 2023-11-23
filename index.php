dc6b5b72e1e5a93271393f297d443232ab105438
danielfabbri <contato.fabbri@gmail.com>
2023-11-23 11:46:59 -0300

    Detalhes

--- index.php
+++ index.php
@@ -13,14 +13,17 @@
 			<form method="POST" action="">
 				<h3><b>Ativar alerta</b></h3>
 				<p>De quantas em quantas horas?</p>
-				<input type="number" name="tempo" required><br>
+				<input type="text" name="tempo" required><br>
 				<button class="btn">Iniciar</button>
 			</form>
 		<?php } else { ?>
 			<h2>Alerta ligado!</h2>
 			<h4>Alerta de <?php echo $_POST['tempo']; ?>h em <?php echo $_POST['tempo']; ?>h</h4>
 			<a href="" class="btn">Reiniciar</a>
-			<script src="index.js"></script>
+			<script>
+				var tempo = parseFloat('<?php echo $_POST['tempo']; ?>');
+			</script>
+			<script src="script.js"></script>
 		<?php } ?>
 	</div>
 </body>
